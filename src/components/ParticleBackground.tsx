7a20efad056f9f0dd9c7968c4b4b2cbd3ddc0d4c
SAMIRAN_47 <98637819+hackerX-Sam@users.noreply.github.com>
2025-10-26 11:42:35 +0530

    Updated ParticleBackground.tsx

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -45,7 +45,7 @@ function ParticleBackground() {
     };
 
     const initializeElements = () => {
-      const particleCount = Math.floor((canvas.width * canvas.height) / 3500);
+      const particleCount = Math.floor((canvas.width * canvas.height) / 7000);
 
       particlesRef.current = Array.from({ length: particleCount }, () => ({
         x: Math.random() * canvas.width,
